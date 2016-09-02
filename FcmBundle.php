b6486f708f989e521a62129d79fb170f7d40bf26
Artem Genvald <genvaldartem@gmail.com>
2016-09-02 23:53:08 +0300

    Init bundle

---
+++ FcmBundle.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * This file is part of the FreshFcmBundle
+ *
+ * (c) Artem Genvald <genvaldartem@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Fresh\FcmBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * FcmBundle.
+ *
+ * @author Artem Genvald <genvaldartem@gmail.com>
+ */
+class FcmBundle extends Bundle
+{
+}
